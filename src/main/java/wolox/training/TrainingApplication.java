f11f28c14fc88857418ae9871b230c42a081752d
Carolina Marulanda <carolina.marulanda@wolox.co>
2021-05-05 12:32:14 -0500

    ## Summary - Completed the book repository ## Trello Card https://trello.com/c/ekXiJvjm/15-crear-un-repositorio-de-books

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -3,7 +3,9 @@ package wolox.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("java.wolox.training.repositories")
 @EntityScan("java.wolox.training.models")
 @SpringBootApplication
 public class TrainingApplication {
