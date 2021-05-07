dc44ea8a93cccb6c9f8e018b4c4adb686687f854
Carolina Marulanda <carolina.marulanda@wolox.co>
2021-05-07 15:19:02 -0500

    ## Summary - User's model and exception for already owner ## Trello Card https://trello.com/c/OvgosXvH/19-crear-modelo-de-user

---
+++ src/main/java/wolox/training/models/UserBook.java
@@ -0,0 +1,76 @@
+package wolox.training.models;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+/**
+ * This class permits manage user´s books
+ */
+public class UserBook {
+
+    /**Username user's books */
+    private String username;
+    /**First name user's books */
+    private String name;
+    /**Date of birthday user's books */
+    private LocalDate birthdate;
+    /**List user's books  */
+    @OneToMany (cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "noveltyRequest")
+    private List<Book> books;
+
+    /**
+     * Empty constructor
+     */
+    public UserBook() {
+    }
+
+    /**
+     * Constructor with parameter of entity User
+     * @param username: user's name
+     * @param name:user's name
+     * @param birthdate: Date of birthday
+     * @param books: List user's books
+     */
+    public UserBook(String username, String name, LocalDate birthdate, List<Book> books) {
+        this.username = username;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.books = books;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public List<Book> getBooks() {
+        return (List<Book>) Collections.unmodifiableList(books);
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
